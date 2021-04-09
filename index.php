f35945ffbaaad25104854d2bcb6c9482910ed8c2
damsover <damien.auversack@outlook.be>
2021-04-09 11:47:10 +0200

    add basic files

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <script type="text/javascript" src="index.js"></script>
+    <title></title>
+</head>
+<body>
+
+</body>
+</html>
