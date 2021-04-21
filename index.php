605f0fe603ccd6d5b8e844b00d3215e83e48d6d9
damsover <damien.auversack@outlook.be>
2021-04-21 14:46:19 +0200

    minor

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="CSS/index.css">
+    <script type="text/javascript" src="JS/index.js"></script>
+    <title></title>
+</head>
+<body>
+
+    <a href="HTML/login.html">login</a>
+    <h3>Hello World !</h3>
+
+</body>
+</html>
