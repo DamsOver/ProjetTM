b6b1250165d3cd907e9a33918846c1790c920426
damsover <damien.auversack@outlook.be>
2021-05-06 14:26:53 +0200

    minor

--- php/config.php
+++ php/config.php
@@ -1,16 +1,16 @@
 <?php
 
     // Config pour 000webhost
-    /*define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'id16658863_root');
-    define('DB_PASSWORD', '@Helha2020Helha');
-    define('DB_NAME', 'id16658863_programmer_bdd');*/
+    /*if (!defined('DB_SERVER')) define('DB_SERVER', 'localhost');
+    if (!defined('DB_USERNAME')) define('DB_USERNAME', 'id16658863_root');
+    if (!defined('DB_PASSWORD')) define('DB_PASSWORD', '@Helha2020Helha');
+    if (!defined('DB_NAME')) define('DB_NAME', 'id16658863_programmer_bdd');*/
 
     // Config local
-    define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'programmer_bdd');
+    if (!defined('DB_SERVER')) define('DB_SERVER', 'localhost');
+    if (!defined('DB_USERNAME')) define('DB_USERNAME', 'root');
+    if (!defined('DB_PASSWORD')) define('DB_PASSWORD', '');
+    if (!defined('DB_NAME')) define('DB_NAME', 'programmer_bdd');
 
     // Connexion à la base de données MySQL
     try {
