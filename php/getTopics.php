68f80891a20deee566db8b66e4c03cd0d53a4a7c
floran <floran.houdart@jsb.be>
2021-05-07 10:56:09 +0200

    Topic card

--- php/getTopics.php
+++ php/getTopics.php
@@ -1,22 +1,46 @@
-<?php
-    include("php/config.php");
-    $theme = $_GET['gTheme'];
-    $req1 = $conn->prepare("select nomtopic from topic where nomtheme ='$theme'");
-    $req1 -> execute();
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    $select = '<ul  class="list-group">';
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css" crossorigin="anonymous">
 
-    if($req1 -> rowCount() > 0){
-        while($data1 = $req1 -> fetch(PDO::FETCH_ASSOC)){
-            $nomTopicTmp = $data1['nomtopic'];
-            $select .= "
-                                <li class='list-group-item'>
-                                    <a href='#'>" . $nomTopicTmp . "</a>
-                                </li>
-                            ";
-        }
-    }
-    $select .= "</ul>";
-    echo $select;
-    $conn = null;
-?>
+        <script type="text/javascript" src="js/index.js"></script>
+        <title></title>
+    </head>
+    <body>
+        <?php
+            include("php/config.php");
+            $theme = $_GET['gTheme'];
+            $req1 = $conn->prepare("select nomtopic from topic where nomtheme ='$theme'");
+            $req1 -> execute();
+
+            $select = '<div  class="row">';
+
+            if($req1 -> rowCount() > 0){
+                while($data1 = $req1 -> fetch(PDO::FETCH_ASSOC)){
+                    $nomTopicTmp = $data1['nomtopic'];
+                    $select .= "
+                                        <div class='col'>
+                                            <div class='card'>
+                                                <div class='card-body'>
+                                                    <h5 class='card-title'>Topic : " . $nomTopicTmp . "</h5>
+                                                    <a href='#' class='stretched-link'>" . $nomTopicTmp . "</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ";
+                }
+            }
+            $select .= "</div>";
+            echo $select;
+            $conn = null;
+        ?>
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="js/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+        <script src="js/popper.min.js" crossorigin="anonymous"></script>
+        <script src="js/bootstrap.min.js" crossorigin="anonymous"></script>
+    </body>
+</html>
