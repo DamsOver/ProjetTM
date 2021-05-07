4d8b760a2bde006eb1bf9983014722943778cc59
damsover <damien.auversack@outlook.be>
2021-05-07 22:25:45 +0200

    minor

--- php/navbar.php
+++ php/navbar.php
@@ -1,6 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php"><img src="img/logo.jpg" width="40" height="30" alt="Site logo">Menu</a>
+        <img style="min-width: 30px;min-height: 40px;" class="navbar-brand" src="img/logo.png" width="30" height="30" alt="Site logo">
+        <a class="navbar-brand" href="index.php">Menu</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent, #navbarconnection" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
