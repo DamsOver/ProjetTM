c9467b13994420c8ab149f187764ade8d879356c
Dotni <57830936+Dotni@users.noreply.github.com>
2021-05-08 10:02:39 +0200

    changements admin

--- php/getThemes.php
+++ php/getThemes.php
@@ -8,9 +8,22 @@ $select = '';
 if($req1 -> rowCount() > 0){
     while($data1 = $req1 -> fetch(PDO::FETCH_ASSOC)){
         $ThemeTmp = $data1['nomtheme'];
-        $select .= '<li class="list-group-item">' .$ThemeTmp .'<button style="float:right;" href="index.php" > supprimer </button></li>';
+
+        $select .= '<li class="list-group-item">' .$ThemeTmp .'
+                <form method ="post" class="form-check-inline" style="float:right;">
+                    <button style="float:right;" name="suppr" value='.$ThemeTmp.'> supprimer </button>
+                </form>
+                </li>';
     }
 }
+
+if (isset($_POST['suppr'])) {
+    $ThemeASup = $_POST['suppr'];
+    $req1 = $conn->prepare("delete from theme where nomtheme = '$ThemeASup'");
+    $req1 -> execute();
+    header("Location: displayAdmin.php");
+}
+
 echo $select;
 $conn = null;
 ?>
