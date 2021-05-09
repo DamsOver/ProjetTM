0c58055a72405533189dc117c2f07422d6ca160d
scryper <florian.mazzeo@gmail.com>
2021-05-09 12:19:56 +0200

    suppression de topic

--- php/getTopics.php
+++ php/getTopics.php
@@ -4,24 +4,41 @@
     $req1 = $conn->prepare("select idtopic, nomtopic, dateajoutTopic from topic where nomtheme ='$theme'");
     $req1 -> execute();
 
+    echo "<h1>$theme</h1>";
     $select = '<div  class="row" style="margin-top: 20px;">';
 
     if($req1 -> rowCount() > 0){
         while($data1 = $req1 -> fetch(PDO::FETCH_ASSOC)){
-            $nomTopicTmp = $data1['nomtopic'];
-            $dateAjoutTopic = $data1['dateajoutTopic'];
-            $idTopicTmp = $data1['idtopic'];
+            $NomTopic = $data1['nomtopic'];
+            $DateAjoutTopic = $data1['dateajoutTopic'];
+            $IdTopic = $data1['idtopic'];
             $select .= "<div class='col-12 col-md-6'>
                             <div class='card'>
                                 <div class='card-body'>
-                                    <p class='card-title'>Topic ouvert le : " . $dateAjoutTopic . "</p>
-                                    <a href='#' id='$idTopicTmp' class='stretched-link'>" . $nomTopicTmp . "</a>
+                                    <p class='card-title'>Topic ouvert le : " . $DateAjoutTopic . "</p>
+                                    <a href='#' id='$IdTopic'>" . $NomTopic . "</a>
+                                    <form  method ='post' class='form-check-inline' style='float:right;'>
+                                        <button style='float:right;' name='supprimerTopic' value=' . $IdTopic . '> Supprimer </button>
+                                    </form>
                                 </div>
                             </div>
                         </div>";
         }
     }
     $select .= "</div>";
+
+    if(isset($_POST['supprimerTopic'])) {
+        $IdTopic = $_POST['supprimerTopic'];
+        if($_SESSION['grade'] >= 2) {
+            $req2 = $conn -> prepare("delete from topic where idtopic = '$IdTopic'");
+            $req2 -> execute();
+        } else {
+            echo "<script>
+                      alert(\"Vous n'êtes pas autorisé à supprimer ce topic.\");
+                  </script>";
+        }
+        header("Location: displayTopics.php?gTheme=" . $theme);
+    }
     echo $select;
     $conn = null;
 ?>
